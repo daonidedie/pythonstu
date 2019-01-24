a339308119a817d8874c72c8f75dbcdcf8ee7c33
daonidedie <daonidedie@sohu.com>
2019-01-24 21:42:14 +0800

    增加了微信代码学习

--- 条件语句学习.py
+++ 条件语句学习.py
@@ -1,3 +1,6 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+import itchat, time
 
 #http://www.runoob.com/python/python-if-statement.html
 
@@ -10,3 +13,40 @@ if False:
     print("flase");
 
 
+
+#https://www.cnblogs.com/wang-li/p/9744502.html
+#利用itchat给女朋友定时发信息
+
+
+
+#itchat.auto_login(loginCallback=lc, exitCallback=ec)
+itchat.auto_login();
+
+def findFriend(NickName):
+    for firend in itchat.get_friends():
+        #print firend["NickName"];
+        if firend["NickName"]==NickName:
+            return firend;
+
+
+#定义用户的昵称
+nickName=u'点点水';
+#查找用户的userid
+#user = itchat.search_friends(send_userid);
+user = findFriend(nickName);
+itcaht_user_name="";
+if user is not None:
+    print (user);
+    itcaht_user_name=user['UserName'];
+
+if user is not None and itcaht_user_name !='':
+    #利用send_msg发送消息
+    itchat.send_msg(u'这是一个测试',toUserName=itcaht_user_name)
+    time.sleep(1000)
+
+itchat.logout()    #强制退出登录   
+  
+# 作者：天府云创 
+# 来源：CSDN 
+# 原文：https://blog.csdn.net/enweitech/article/details/79585043 
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
