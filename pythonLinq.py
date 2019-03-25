50aaeeaa0d7194ef33dc504c511060361386574c
daonidedie <daonidedie@sohu.com>
2019-03-25 22:17:06 +0800

    学习 asq  lambda

---
+++ pythonLinq.py
@@ -0,0 +1,18 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+#Linq 
+#https://pypi.org/project/asq/
+ 
+import random
+from asq import query;
+ 
+abc= ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+#print(query);
+print query(abc).order_by(len).then_by().take(5).select(str.upper).to_list()
+#打印长度大于3的所有值
+for item in  query(abc).where(lambda p: len(p)>3).to_list():
+     print item  
+
+
+
