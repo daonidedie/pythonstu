9e9ce7d8268cd882ec3727c4287767eb8c23f6b8
daonidedie <daonidedie@sohu.com>
2019-03-25 22:24:42 +0800

    python Lambda study

--- pythonLinq.py
+++ pythonLinq.py
@@ -6,11 +6,11 @@
  
 import random
 from asq import query;
- 
+  
 abc= ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 #print(query);
 print query(abc).order_by(len).then_by().take(5).select(str.upper).to_list()
-#打印长度大于3的所有值
+#打印长度大于3的所有值 sfds
 for item in  query(abc).where(lambda p: len(p)>3).to_list():
      print item  
 
